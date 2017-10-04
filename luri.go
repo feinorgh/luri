b693cf2274e5e40e8ff0834a59855543a8c19233
Pär Karlsson <feinorgh@gmail.com>
2017-10-04 20:57:56 +0200

    Make COPYING the proper file

--- luri.go
+++ luri.go
@@ -1,17 +1,113 @@
+/*
+luri - list unique random integers
+
+Copyright (C) 2017  Pär Karlsson <feinorgh@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"math/big"
-	// "os"
+	"math/rand"
+	"os"
+	"time"
 )
+
+type Node struct {
+	Number big.Int
+	Left   *Node
+	Right  *Node
+}
+
+type Tree struct {
+	Root *Node
+}
 
 type Options struct {
 	lowerBound *big.Int
 	upperBound *big.Int
 	count      int
 	verbose    bool
+}
+
+func (n *Node) Insert(number *big.Int) error {
+	if n == nil {
+		return errors.New("Cannot insert a value into a nil tree")
+	}
+	c := number.Cmp(&n.Number)
+	switch {
+	case c == 0:
+		return nil
+	case c < 0:
+		if n.Left == nil {
+			n.Left = &Node{Number: *number}
+			return nil
+		}
+		return n.Left.Insert(number)
+	case c > 0:
+		if n.Right == nil {
+			n.Right = &Node{Number: *number}
+			return nil
+		}
+		return n.Right.Insert(number)
+	}
+	return nil
+}
+
+func (n *Node) Find(number *big.Int) bool {
+	if n == nil {
+		return false
+	}
+	c := number.Cmp(&n.Number)
+	switch {
+	case c == 0:
+		return true
+	case c < 0:
+		return n.Left.Find(number)
+	default:
+		return n.Right.Find(number)
+	}
+
+}
+
+func (t *Tree) Insert(number *big.Int) error {
+	if t.Root == nil {
+		t.Root = &Node{Number: *number}
+		return nil
+	}
+	return t.Root.Insert(number)
+}
+
+func (t *Tree) Find(number *big.Int) bool {
+	if t.Root == nil {
+		return false
+	}
+	return t.Root.Find(number)
+}
+
+func (t *Tree) Traverse(n *Node, f func(*Node)) {
+	if n == nil {
+		return
+	}
+	t.Traverse(n.Left, f)
+	f(n)
+	t.Traverse(n.Right, f)
 }
 
 func setOptions(opt *Options) {
@@ -48,12 +144,33 @@ func setOptions(opt *Options) {
 func main() {
 	var opt Options
 	setOptions(&opt)
-	args := flag.Args()
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	if opt.verbose {
 		fmt.Println("Lower Bound: ", opt.lowerBound)
 		fmt.Println("Upper Bound: ", opt.upperBound)
+		fmt.Println("Count: ", opt.count)
 		fmt.Printf("%+v\n", opt)
-		fmt.Println(opt)
-		fmt.Println(args)
 	}
+
+	i := new(big.Int)
+	i.Set(i.Sub(opt.upperBound, opt.lowerBound))
+	if c := i.Cmp(big.NewInt(int64(opt.count))); c < 0 {
+		fmt.Fprintf(os.Stderr, "Error: the interval (%s) is less than requested size of set (%d)\n", i, opt.count)
+		os.Exit(1)
+	}
+	tree := &Tree{}
+	for n := 0; n < opt.count; n++ {
+		for {
+			x := new(big.Int)
+			x.Rand(r, i)
+			x.Add(x, opt.lowerBound)
+			if tree.Find(x) == false {
+				tree.Insert(x)
+				break
+			}
+		}
+	}
+	tree.Traverse(tree.Root, func(n *Node) {
+		fmt.Println(n.Number.String())
+	})
 }
